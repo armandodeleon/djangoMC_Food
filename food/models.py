54007017fb68717e4a5cc11660ee6d7152589cd6
Armando De Leon Del Bosque <armandodeleon@outlook.com>
2024-01-06 14:25:55 -0600

    Section6: basic template. Django Master Class Build 9 Real Django Projects Udemy

---
+++ food/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Item(models.Model):
+
+    def __str__(self):  # object representation
+        return self.item_name
+
+    item_name = models.CharField(max_length=200)
+    item_description = models.CharField(max_length=200)
+    item_price = models.DecimalField(max_digits=4, decimal_places=2)
