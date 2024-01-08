d668887ce718b291d53d73d61887f365ace74526
Armando De Leon Del Bosque <armandodeleon@outlook.com>
2024-01-07 18:21:23 -0600

    Section6: video 26. Detail View based on item_id

--- food/urls.py
+++ food/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),     # /food/
+    path('<int:item_id>/', views.detail),    # /food/1
     path('item/', views.item, name='item')
 ]
+
--- food/views.py
+++ food/views.py
@@ -7,12 +7,15 @@ from django.template import loader
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    template = loader.get_template('food/index.html')
     context = {
-
+        'item_list': item_list,
     }
-    return HttpResponse(template.render(context,request))
+    return render(request, 'food/index.html', context)
 
 
 def item(request):
     return HttpResponse("<h1>This is an item view<h1>")
+
+
+def detail(request, item_id):
+    return HttpResponse(f"This is the page: {item_id}")
