d49e8105f3b23b452e0b0713fdd4c0527fbed00e
Armando De Leon Del Bosque <armandodeleon@outlook.com>
2024-01-09 07:22:25 -0600

    36. Image field to model and index Bootstrap style

--- food/models.py
+++ food/models.py
@@ -11,4 +11,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_description = models.CharField(max_length=200)
     item_price = models.DecimalField(max_digits=4, decimal_places=2)
-
+    item_image = models.CharField(max_length=500, default='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfdIv7mYroRcEro01QihNy4FertDfXFWtetQ&usqp=CAU')
